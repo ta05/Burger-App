76ed04ff0138091fe733f4f4bf8a169b58e8155d
Tolu Alimi <tolualimi05@gmail.com>
2020-07-14 14:10:39 -0500

    Finished controller file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,4 +4,32 @@ const router = express.Router();
 
 const burger = require("../models/burger");
 
+
+router.get("/", function (req, res) {
+    burger.select(function (data) {
+        let hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("api/burgers", function (req, res) {
+    burger.insert(["burger_name"], [req.body.name], function (result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    let condition = `id = ${req.params.id}`;
+
+    console.log("condition", condition);
+
+    burger.update({ devoured: req.body.devoured }, condition, function (result) {
+        if (result.changedRows === 0)
+            return res.status(404).end();
+        res.status(200).end();
+    });
+});
 module.exports = router;
